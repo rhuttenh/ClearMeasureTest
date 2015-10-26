c311e9d3a131ee3985e67da645bb05fdf29231d1
Robbie Huttenhower <robbie@thehuttenhowers.com>
2015-10-26 17:02:38 -0500

    Added Console Project

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using SpecialCounter;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            SpecialList specialList = new SpecialList
+            {
+                Max = 1000,
+                CounterHighlights = new Dictionary<int, string>() {{5, "Fun"}, {6, "More Fun"}}
+            };
+            Dictionary<int, string> list = specialList.Generate();
+            foreach (var item in list)
+            {
+                Console.WriteLine(item.Value);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
