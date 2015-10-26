6c87a0e2fbf23905e7b085158d8e0bd3a47377ba
Robbie Huttenhower <robbie@thehuttenhowers.com>
2015-10-26 17:03:04 -0500

    Added Solution

---
+++ SpecialCounter/SpecialList.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace SpecialCounter
+{
+    public class SpecialList
+    {
+        public Dictionary<int, string> CounterHighlights { get; set; } = new Dictionary<int, string>() { { 3, "Fizz" }, { 5, "Buzz" } };
+
+        public int Max { get; set; } = 100;
+
+        public Dictionary<int, string> Generate()
+        {
+            Dictionary<int, string> output = new Dictionary<int, string>();
+            for (var i = 1; i <= Max; i++)
+            {
+                var foundatleastone = false;
+                var outputstring = string.Empty;
+                foreach (var pair in CounterHighlights)
+                {
+                    if (i % pair.Key == 0)
+                    {
+                        outputstring += foundatleastone ? " " + pair.Value : pair.Value;  
+                        foundatleastone = true;
+                    }
+                }
+                outputstring = foundatleastone ? outputstring : i.ToString();
+                output.Add(i,outputstring);
+            }
+            return output;
+        }
+    }
+}
---
+++ SpecialCounterTests/SpecialListTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SpecialCounter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpecialCounter.Tests
+{
+    [TestClass()]
+    public class SpecialListTests
+    {
+        [TestMethod()]
+        public void GenerateTestusingDefaults()
+        {
+            SpecialList list = new SpecialList();
+            Dictionary<int, string> newlist = list.Generate();
+            Assert.IsTrue(newlist.Count == 100);
+        }
+
+        [TestMethod()]
+        public void GenerateTestusing5andFunwithMaxof50()
+        {
+            SpecialList list = new SpecialList();
+            list.CounterHighlights = new Dictionary<int, string>() { { 5, "Fun" } };
+            list.Max = 50;
+            Dictionary<int, string> newlist = list.Generate();
+            Assert.IsTrue(newlist.Count == 50);
+            string outstring = string.Empty;
+            newlist.TryGetValue(5, out outstring);
+            Assert.IsTrue(outstring == "Fun");
+        }
+    }
+}
